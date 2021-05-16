60ece28f37215d27b544e388ea2b209b29f97b8b
AnthonyM5 <anthony.mai530@gmail.com>
2021-05-15 23:39:45 -0400

    adding file structure

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Focus } from './src/features/focus/Focus'
 
-// You can import from local files
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 export default function App() {
-  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-    <Text>Hello World!</Text>
-    <TextInput
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
-      />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {text.split(' ').map((word) => word && '🍕').join(' ')}
-      </Text>
+    <View style={styles.titleContainer}>
+    <Text style={styles.title}>Hello World!</Text>
+    </View>
+    <Focus />
     </View>
   );
 }
@@ -30,6 +20,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
+  },
+  titleContainer: {
+    flexDirection: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
   }
   
 });
---
+++ src/features/focus/Focus.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
+
+
+export const Focus = () => {
+  const [text, setText] = useState('');
+  console.log(text)
+  return (
+    <View style={styles.container}>
+    <Text>Emojifier!</Text>
+    <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join('')}
+        Word Count: {text.split(' ').length > 1 ? text.split(' ').length : 0}
+      </Text>
+    </View>
+    
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  }
+  
+});
