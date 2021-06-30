d869d369c453e6138cc3f800befef3bc13f02db3
AnthonyM5 <anthony.mai530@gmail.com>
2021-06-30 19:30:35 -0400

    countdown - moved onEnd from setMillis to useeffect

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,62 +1,60 @@
-import React, { useState, useEffect} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 import { fontSizes, spacing } from '../utils/Sizes';
 import { colors } from '../utils/Colors';
 
+const minutesToMil = (min) => min * 1000 * 60;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
-const minutesToMil = (min) => min * 1000 * 60
-const formatTime = (time) => time < 10 ? `0${time}` : time
-
-
-export const Countdown = ({
-  minutes = 20,
-  onProgress,
-  isPaused,
-  onEnd
-}) => {
-  const interval = React.useRef(null)
+export const Countdown = ({ minutes = 20, onProgress, isPaused, onEnd }) => {
+  const interval = React.useRef(null);
+  const [millis, setMillis] = useState(minutesToMil(minutes));
 
   const countDown = () => {
     setMillis((time) => {
-      if(time === 0) {
-    // invoke  onEnd 
-        clearInterval(interval.current)
-        onEnd()
-      return time
+      if (time === 0) {
+        // move onEnd to useEffect outside of this component's setState
+        clearInterval(interval.current);
+        // onEnd();
+        return time;
       }
-      const timeLeft = time - 1000
-    // report progress
-      return timeLeft
-    })
-  }
+      const timeLeft = time - 1000;
+      // report progress
+      return timeLeft;
+    });
+  };
 
   useEffect(() => {
-    setMillis(minutesToMil(minutes))
-  }, [minutes])
+    setMillis(minutesToMil(minutes));
+  }, [minutes]);
 
   useEffect(() => {
-    onProgress(millis / minutesToMil(minutes))
-  }, [millis])
+    onProgress(millis / minutesToMil(minutes));
+    if (millis === 0) {
+      onEnd()
+    }
+  }, [millis]);
 
   useEffect(() => {
     if (isPaused) {
-      if (interval.current) clearInterval(interval.current)
-      return
+      if (interval.current) clearInterval(interval.current);
+      return;
     }
-    interval.current = setInterval(countDown, 1000)
-    return () => clearInterval(interval.current)
-  }, [isPaused])
+    interval.current = setInterval(countDown, 1000);
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
 
-  const [millis, setMillis] = useState(minutesToMil(minutes))
 
-  const minute = Math.floor(millis / 1000 / 60) % 60
-  const seconds = Math.floor(millis / 1000 ) % 60
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
 
   return (
-    <Text style={styles.text}>{formatTime(minute)}:{formatTime(seconds)}</Text>
-  )
-}
+    <Text style={styles.text}>
+      {formatTime(minute)}:{formatTime(seconds)}
+    </Text>
+  );
+};
 
 const styles = StyleSheet.create({
   text: {
@@ -64,8 +62,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: colors.white,
     padding: spacing.lg,
-    backgroundColor: 'rgba(50,205,255, 0.3)'
-  }
-})
-
-
+    backgroundColor: 'rgba(50,205,255, 0.3)',
+  },
+});
